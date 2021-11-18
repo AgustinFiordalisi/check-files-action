2f08c040a13ae09a7749c469464b35bab3b45eea
AgustinFiordalisi <agustin.fiordalisi@mercadolibre.com>
2021-11-18 16:07:20 -0300

    fix: index add -> raw_detail

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
-const { Octokit } = require("@octokit/core");
-const octokit = new Octokit({ auth: `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` });
 
 
 async function checkFileExists(filePath) {
@@ -45,13 +43,14 @@ async function checkFileStartsWithHeader(filePath) {
                 ! await checkFileStartsWithHeader("README.md")
             ) {
                 // get token for octokit
-                //const token = core.getInput('repo-token');
-                //const octokit = new github.getOctokit(token);
+                const token = core.getInput('repo-token');
+                const octokit = new github.getOctokit(token);
 
-                const response = await octokit.request('POST /repos/{owner}/{repo}/check-runs', {
+                // call octokit to create a check with annotation and details
+                const check = await octokit.rest.checks.create({
                     owner: github.context.repo.owner,
                     repo: github.context.repo.repo,
-                    name: 'Check Agus',
+                    name: 'Readme Validator',
                     head_sha: github.context.sha,
                     status: 'completed',
                     conclusion: 'failure',
@@ -60,18 +59,22 @@ async function checkFileStartsWithHeader(filePath) {
                         summary: 'Please use markdown syntax to create a title',
                         annotations: [
                             {
+
                                 path: 'README.md',
                                 start_line: 1,
                                 end_line: 1,
                                 annotation_level: 'failure',
                                 message: 'README.md must start with a header',
                                 start_column: 1,
-                                end_column: 1
+                                end_column: 1,
+                                raw_details: 'Aca se ven los detalles del check...'
                             }
                         ]
                     }
-                  });
-                // call octokit to create a check with annotation and details
+                });
+
+
+
             }
 
 
